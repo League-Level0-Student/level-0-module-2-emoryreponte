98dc450977aa076f1991bd2cc2391233e0f8aead
lvl0_module2 <Eddiejhunter1000000@gmail.com>
2026-01-28 18:11:30 -0800

    I Finished _04_spot_the_error

--- src/_04_spot_the_error/HowMany.java
+++ src/_04_spot_the_error/HowMany.java
@@ -9,13 +9,13 @@ public class HowMany {
 	// Change one line of code to fix this program.
 	
 	public static void main(String[] args) {
-		int number = 0;
+		int number = getRandomNumber();
 		System.out.println(number);
 	}
 	
 	public static int getRandomNumber() {
 		Random random = new Random();
-		int number = random.nextInt(100)+1;
+		int number = random.nextInt(100 - 1 + 1)+1;
 		return number;
 	}
 }
